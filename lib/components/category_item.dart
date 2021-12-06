72a780017cf66a6abd4747dd08dcc5e9171a8de8
Danilo Valério da Silva <danilo_vs@hotmail.com>
2021-12-06 15:07:21 -0300

    Navigator para avancar tela

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:receitas/models/category.dart';
+import 'package:receitas/screens/categories_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   final Category category;
@@ -10,23 +11,39 @@ class CategoryItem extends StatelessWidget {
     required this.category,
   }) : super(key: key);
 
+  void _selecionaCategoria(BuildContext ctx) {
+    Navigator.of(ctx).push(
+      MaterialPageRoute(
+        builder: (_) {
+          return CategoriesMealsScreen();
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(12),
-      child: Text(
-        category.title,
-        style: Theme.of(context).textTheme.caption,
-      ),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15),
-        gradient: LinearGradient(
-          colors: [
-            category.color.withOpacity(0.5),
-            category.color,
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    ///INKWELL PERMITE INTERAGIR QUANDO TOCAR SOBRE onTap
+    return InkWell(
+      onTap: () => _selecionaCategoria(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: EdgeInsets.all(12),
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.caption,
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.5),
+              category.color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
         ),
       ),
     );
